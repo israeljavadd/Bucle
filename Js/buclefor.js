0f97d41596d9f6689fb9a0845a6b935a271876a6
Israeljavadd <israeljavadd@gmail.com>
2022-03-30 17:56:32 -0600

    Bucle

---
+++ Js/buclefor.js
@@ -0,0 +1,9 @@
+for(var vuelta=0;vuelta <=50; vuelta++){
+    if(vuelta>=2){
+        document.write(" "+vuelta+" Vueltas");   
+    }else{
+        document.write(" "+vuelta+" Vuelta");   
+    }
+    
+
+}
---
+++ Js/buclewhile.js
@@ -0,0 +1,8 @@
+var conteo=0;
+
+while(conteo<=50){
+    document.write(" "+conteo+" segundos ");
+   
+    conteo++;
+}
+
